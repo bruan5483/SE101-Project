661820cc9711a34d26649e8acd6fa1050ac62bf6
Ray Wang <ray.wang2@uwaterloo.ca>
2024-11-05 14:16:39 -0500

    mergeutil v1 (ability to merge changes from a text file into the desired codefile)

---
+++ codestuff.c
@@ -0,0 +1,7 @@
+#include <stdbool.h>
+#include <stdio.h>
+int main() { //comments yippee
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
---
+++ mergeutil.py
@@ -0,0 +1,58 @@
+def parse_mergefile(toedit,mergefile):
+    readingcode = open(toedit,"r")
+    cached_codefile=readingcode.readlines()
+    readingcode.close()
+
+    print (cached_codefile)
+
+    updated_codefile=cached_codefile
+
+    with open(mergefile) as tomerge:
+        for line in tomerge:
+            print(line)
+            parsedline=parse_line(line.rstrip())
+            print(parsedline)
+            updated_codefile=update_contents(parsedline,updated_codefile)
+    
+    print (updated_codefile)
+    update_codefile(updated_codefile,toedit)
+
+def parse_line(l)->(int,int,str):
+    splitstring=l.split(',')
+    #add error detection
+    linenum=int(splitstring[0])
+    index=int(splitstring[1])
+
+    stuff=splitstring[2]
+    for i in range(3,len(splitstring)):
+        stuff+=","+splitstring[i]
+
+    stuff=stuff.replace("[newline]", "\n")
+
+    stuff=stuff.replace("[comment]", " //") #relace with function to get correct comment characters
+
+    #add other parsing features
+
+    return (linenum,index,stuff)
+
+def update_contents(tochange:tuple[int,int,str],contents):
+    if (tochange[1]==-1):
+        contents[tochange[0]]=contents[tochange[0]].rstrip()
+        contents[tochange[0]]+=tochange[2]
+
+    elif (tochange[1]==0):
+        contents.insert(tochange[0]-1,tochange[2])
+    
+    #else:
+        #handle inserting to the middle of a line
+    
+    return contents
+
+def update_codefile(contents,writepath):
+    with open(writepath,"w") as writefile:
+        writefile.writelines(contents)
+
+file_to_edit="codestuff.c"
+
+
+parse_mergefile(file_to_edit,"tomerge.txt")
