ba19d84d43d3e7cfec9ab7bb8efa04bdb5b631ed
RoyZ1017 <zroy1017@gmail.com>
2024-11-05 13:50:10 -0500

    split up code file

---
+++ src/screenshot.py
@@ -0,0 +1,57 @@
+from PIL import ImageGrab
+import pyautogui
+from screeninfo import get_monitors
+import keyboard
+import time 
+import os 
+
+"""
+Dependencies:
+- pip install pillow
+- pip install pyautogui
+- pip install screeninfo
+- pip install keyboard
+"""
+
+# stops program if more 1 minute is taken 
+MAX_ELAPSE_TIME = 60
+
+# get size of screen
+monitor = get_monitors()[0]
+width = monitor.width
+height = monitor.height
+print(f"Screen resolution: {width}x{height}")
+def scroll(height):
+    pyautogui.scroll(height)
+
+
+
+file_path = "C:\\Users\\zroy1\\SE101\\se101-f2024-triangles\\triangle.c"
+os.system(f'code {file_path}')
+start_time = time.time()
+
+
+def image_to_bytes(image):
+    return list(image.getdata())
+
+count = 1
+prev_screenshot = None
+while time.time() - start_time < MAX_ELAPSE_TIME:
+    print(count)
+    # fail safe, stops program if space bar is pressed 
+    if keyboard.is_pressed('ctrl'):
+        break
+
+    # take screenshot 
+    screenshot = ImageGrab.grab(bbox =(0.04 * width, 0.075 * height, 0.85 * width, 0.95 * height))
+    # if the two screenshots are the same that means we've reached the end of our scrollable area 
+    if image_to_bytes(screenshot) == prev_screenshot:
+        print("stopped")
+        break
+
+    prev_screenshot = image_to_bytes(screenshot)
+    img_path = f"..\\screenshots\\pic{count}.jpg"
+    screenshot.save(img_path)
+    scroll(-height)
+    count += 1
+    time.sleep(0.1)
