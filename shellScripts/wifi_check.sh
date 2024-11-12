e5593848644a923b969fd024b6afc90382e11dcc
Rosnaky <ronakjpatelr22@gmail.com>
2024-11-12 14:02:31 -0500

    display screenshot file

--- shellScripts/wifi_check.sh
+++ shellScripts/wifi_check.sh
@@ -4,7 +4,7 @@
 while true; do
         if ping -c 1 google.com &> /dev/null; then
 		echo "Connected to Wi-Fi. Running Python script"
-		python3 /home/ronak/Desktop/project-code/src/fileUpload.py
+		python3 /home/ronak/Desktop/project-code/src/app.py
 		break
 	else
 		echo "Not connected to Wi-Fi. Retrying in 5 seconds..."
