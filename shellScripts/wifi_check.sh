666fff563ec4925804461cff15dbb780d564cea3
rpi <r93patel@uwaterloo.ca>
2024-11-21 21:44:18 +0000

    added dir paths to .env

--- shellScripts/wifi_check.sh
+++ shellScripts/wifi_check.sh
@@ -4,9 +4,11 @@ while true; do
         if ping -c 1 google.com &> /dev/null; then
 		echo "Connected to Wi-Fi. Running Python script"
 		echo "Activating venv"
+		source /home/ronak/Desktop/project-code/venv/bin/activate
+		echo "Creating vnc server"
+		tigervncserver
 		export DISPLAY=:1
 		xhost +
-		source /home/ronak/Desktop/project-code/venv/bin/activate
 		python3 /home/ronak/Desktop/project-code/src/app.py
 		break
 	else
