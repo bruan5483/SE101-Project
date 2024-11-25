a94f7d806af8b908f2634575f2dca02127d73869
Haris Khawja <hkhawja@uwaterloo.ca>
2024-11-25 18:31:00 -0500

    commented out another roy "oopsie"
    Former-commit-id: 1fa9bdf1c024fe6ff15aa2da230e98875daf6dcf

--- src/generateMergeFile.py
+++ src/generateMergeFile.py
@@ -58,7 +58,7 @@ def generateToMerge(ocr_file, img_directory, tomerge_path):
     return tomerge_path
 
 # change paths 
-generateToMerge("C:\\Users\\zroy1\\SE101\\se101-team-21\\utils\\ocr.txt", 
-                "C:\\Users\\zroy1\\SE101\\se101-team-21\\utils\\images",
-                TOMERGE_PATH)
+# generateToMerge("C:\\Users\\zroy1\\SE101\\se101-team-21\\utils\\ocr.txt", 
+#                 "C:\\Users\\zroy1\\SE101\\se101-team-21\\utils\\images",
+#                 TOMERGE_PATH)
 
